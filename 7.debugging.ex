41a0cae969ff4c5afe30d9650fea131bec1e0bef
Mike Andrianov <mikeandrianov@gmail.com>
2020-12-04 12:35:22 +0100

    Update 7.debugging.ex

--- 7.debugging.ex
+++ 7.debugging.ex
@@ -3,6 +3,10 @@
 # Debugging!
 IO.inspect()
 
+# Debugging with pry
+require IEx
+IEx.pry
+
 # Logging
 defmodule MyModule do
   # https://elixir-lang.org/getting-started/alias-require-and-import.html
